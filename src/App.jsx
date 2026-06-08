dccc540e02d869cfa9c6fcf017bde0a367ec76b0
Donatasval <valainis.donatas@gmail.com>
2026-06-08 19:56:50 +0300

    Puslapiai kraunami tiesiai is src

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { BookingProvider } from "./context/BookingContext.jsx";
 
-const LandingPage = lazy(() => import("./pages/LandingPage.jsx"));
-const AdminPage = lazy(() => import("./pages/AdminPage.jsx"));
+const TrainerLanding = lazy(() => import("./TrainerLanding.jsx"));
+const TrainerAdmin = lazy(() => import("./TrainerAdmin.jsx"));
 
 export default function App() {
   return (
     <BookingProvider>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/" element={<TrainerLanding />} />
+        <Route path="/admin" element={<TrainerAdmin />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BookingProvider>
